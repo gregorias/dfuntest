881090eefe3a4b042c11db8f5437f8a642912a32
Grzegorz Milka <grzegorzmilka@gmail.com>
2014-09-10 10:13:29 -0700

    Add comments.

--- src/main/java/org/nebulostore/dfuntesting/ApplicationFactory.java
+++ src/main/java/org/nebulostore/dfuntesting/ApplicationFactory.java
@@ -1,5 +1,11 @@
 package org.nebulostore.dfuntesting;
 
 public interface ApplicationFactory<TestedApp extends App> {
+  /**
+   * Create new application proxy.
+   *
+   * @param env Environment in which the application runs.
+   * @return Application
+   */
   TestedApp newApp(Environment env);
 }
--- src/main/java/org/nebulostore/dfuntesting/Environment.java
+++ src/main/java/org/nebulostore/dfuntesting/Environment.java
@@ -8,23 +8,48 @@ import java.util.NoSuchElementException;
 /**
  * Class which represents the environment on which given application is run,
  * such as local or remote host.
+ *
  * It tries to provide uniform console-like environment independent of what kind of environment
  * it is.
  */
 public interface Environment {
 
+  /**
+   * @param srcPath Local source path.
+   * @param destRelPath Relative destination path on this environment.
+   * @throws IOException
+   */
   void copyFilesFromLocalDisk(Path srcPath, Path destRelPath) throws IOException;
 
+  /**
+   * @param srcRelPath Relative source path on this environment.
+   * @param destPath Local destination path.
+   * @throws IOException
+   */
   void copyFilesToLocalDisk(Path srcRelPath, Path destPath) throws IOException;
   
+  /**
+   * @return Hostname of this environment. May be IP address, WWW address etc.
+   */
   String getHostname();
 
+  /**
+   * @return Numeric identifier of this environment.
+   */
   int getId();
 
   String getName();
 
   Object getProperty(String key) throws NoSuchElementException;
 
+  /**
+   * Synchronously runs arbitrary command on this environment's OS.
+   *
+   * @param command
+   * @return
+   * @throws CommandException
+   * @throws InterruptedException
+   */
   CommandResult runCommand(List<String> command) throws CommandException, InterruptedException;
 
   Process runCommandAsynchronously(List<String> command) throws CommandException;
